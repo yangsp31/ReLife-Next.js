e22ea75fdeba7ce78e44d4a9bc1e3c84fe6b9ede
heyryung <happy79891@gmail.com>
2024-05-27 22:10:47 +0900

    about페이지 사진추가

--- src/app/(pages)/about/page.js
+++ src/app/(pages)/about/page.js
@@ -3,7 +3,6 @@ import styles from "../../page.module.css";
 export default function Component() {
   return (
     <div className={`${styles.main} ${styles.flex} ${styles.flexCol} ${styles.itemsCenter} ${styles.justifyCenter} ${styles.minHScreen}`} >
-
     <div
       className="text-white p-12 flex justify-center"
       style={{
@@ -14,15 +13,21 @@ export default function Component() {
     >
       <div className="flex flex-col items-center space-y-8">
         <h1 className="text-5xl font-bold">목적</h1>
-          <div className={styles.aboutBox}>
-            <h2 className="text-xl font-semibold">머리로만 상상하는것은 그만, 상상을 눈으로 보며 계획해보세요.</h2>
+        <div className={`${styles.aboutBox} ${styles.flex} ${styles.flexCol} ${styles.itemsCenter}`}>
+          <h2 className="text-xl font-semibold">머리로만 상상하는것은 그만, 상상을 눈으로 보며 계획해보세요.</h2>
+          <div className={styles.imageContainer1}>
+            <img src="/home-cover-1.jpg" alt="Home Cover" className={styles.image1} />
           </div>
-          <div className="flex flex-col items-center space-y-4">
-            <h1 className="text-xl font-semibold">A.I</h1>
-            <h2 className="text-xl font-semibold">ReimagineHome A.I 서비스의 API를 사용 하였습니다.</h2>
+        </div>
+        <div className="flex flex-col items-center space-y-4">
+          <h1 className="text-xl font-semibold">A.I</h1>
+          <h2 className="text-xl font-semibold">ReimagineHome A.I 서비스의 API를 사용 하였습니다.</h2>
+          <div className={styles.imageContainer1}>
+            <img src="/reimagine.jpg" alt="Home Cover" className={styles.image1} />
           </div>
         </div>
       </div>
     </div>
+  </div>
   )
 }
